1769939fe87b79d30425813be2ce289fb3a6bfdf
EC2 Default User <ec2-user@ip-10-0-0-41.ec2.internal>
2018-10-09 09:20:35 +0000

    vpc

--- main.tf
+++ main.tf
@@ -2,3 +2,30 @@ provider "aws" {
   profile    = "${var.profile-name}"
   region     = "${var.aws-region}"
 }
+resource "aws_vpc" "dev_vpc" {
+  cidr_block       = "10.0.0.0/16"
+
+  tags {
+    Name = "main"
+  }
+}
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "${aws_vpc.dev_vpc.id}"
+
+  tags {
+    Name = "dev-igw"
+  }
+}
+resource "aws_route_table" "public-route" {
+  vpc_id = "${aws_vpc.dev_vpc.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.gw.id}"
+  }
+
+ 
+  tags {
+    Name = "public-route"
+  }
+}
